1b6b37d20896f4da16b34420ab6fa88fd02494d6
Albert Tu <albeetu@gmail.com>
2015-01-15 11:19:14 -0800

    Build the ORR compliant machine and add the start of the process manager

---
+++ BuildORRReporting.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+1) Process raw data
+1a) xls convert to csvs
+CISO contacts
+holding pen extracts
+SCD extract
+
+2) Create orr asset buckets in csv format
+2a) compliant bucket
+2b) non compliant bucket
+
+Create asset stories for non-compliant devices.
+
+3) Create analysis reports
+
+3a) holding pen dashboarding
+b) out of compliance dashboard
+c) compliant dashboard
+d) ORR trending report
+
+*/
+
+class runORRProcess()
+{
+
+public __construct()
+{
+  #configuration string?
+  # date window?
+}
+
+}
