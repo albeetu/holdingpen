bc2d781451304fc30976efe902e22a7a731bb3be
Albert Tu <albeetu@gmail.com>
2015-01-15 13:13:33 -0800

    add more functionality to buildORRCompliantList.php

--- buildORRCompliantList.php
+++ buildORRCompliantList.php
@@ -1,6 +1,40 @@
 <?php
+
+require_once 'buildSCDExtract.php';
+require_once 'hostProfileStory.php';
+require_once 'lib/utils.inc.php';
+
 class orrCompliantAssets
 {
+  
+  protected $unique_assets;
+  protected $assets_not_in_SCD;
+  protected $scd;
+  
+  public function __construct($orrCompliantFile, $SCDList = null)
+  {
+    $this->scd = $SCDList;
+    print("--Importing RAW ORR Compliant Extract --\n");
+    $this->compliantCSV = new parseCSV($orrCompliantFile);
+    $this->unique_assets = array_unique(array_column($this->compliantCSV->data,"Host Name"));
+    $this->orr_filename = $orrCompliantFile;
+  }
+
+  public function dashboard()
+  {
+    print("--ORR Compliant Assets Dashboard--\n");
+    ## count the number of records
+    print(count($this->compliantCSV->data)." records in compliant ORR list.\n");
+    ## count the number of unique hosts
+    print(count($this->unique_assets). " unique hosts in compliant ORR list.\n");
+    ## scrubed count against SCD
+    print(count($this->assets_not_in_SCD)." hosts recommended to be added to SCD.\n");
+  }
+  
+  protected function unique_list_SCDscrubbed()
+  {
+
+  } 
 
 # build list
 # build stats
@@ -9,4 +43,7 @@ class orrCompliantAssets
 # 
 
 }
-?>
+
+print ("------Building compliant ORR Asset report ------\n");
+$orrCompliantList = new orrCompliantAssets("orrCompliantPresent.csv");
+$orrCompliantList->dashboard();
