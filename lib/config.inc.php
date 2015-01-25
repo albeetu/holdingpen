6b9fa4da4eda352e76f6e33cc102983dc1090052
Albert Tu <albeetu@gmail.com>
2015-01-24 19:44:22 -0800

    Add extract filelist to config.inc.php

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -13,3 +13,26 @@ $config['CISOExtract'] = "CISOContact.csv";
 $config['CompliantListName'] = "orrCompliantPresent.csv";
 $config['NonCompliantListName'] = "orrNonCompliantPresent.csv";
 
+#filelist
+
+$config['NonCompliantList'] = Array(
+"output/orrExtracts/orrExtract11-24-2014-noncompliant.csv",
+"output/orrExtracts/orrExtract12-3-2014-noncompliant.csv",
+"output/orrExtracts/orrExtract12-19-2014-noncompliant.csv",
+"output/orrExtracts/orrExtract1-9-2015-noncompliant.csv",
+"output/orrExtracts/orrExtract1-13-2015-noncompliant.csv",
+"output/orrExtracts/orrExtract1-19-2015-noncompliant.csv",
+"output/orrExtracts/orrExtract1-22-2015-noncompliant.csv"
+);
+
+$config['Compliant'] = Array(
+"output/orrExtracts/orrExtract11-24-2014-compliant.csv",
+"output/orrExtracts/orrExtract12-3-2014-compliant.csv",
+"output/orrExtracts/orrExtract12-19-2014-compliant.csv",
+"output/orrExtracts/orrExtract1-9-2015-compliant.csv",
+"output/orrExtracts/orrExtract1-13-2015-compliant.csv",
+"output/orrExtracts/orrExtract1-19-2015-compliant.csv",
+"output/orrExtracts/orrExtract1-22-2015-compliant.csv"
+);
+
+
