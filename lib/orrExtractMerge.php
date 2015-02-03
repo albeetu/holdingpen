c0019da29a449f2156e6e56e592eae8d733c6d92
Albert Tu <albeetu@gmail.com>
2015-02-03 07:48:06 -0800

    fix printing issue with csvmerge function

--- lib/orrExtractMerge.php
+++ lib/orrExtractMerge.php
@@ -6,12 +6,15 @@ class orrMergeExtract
 {
   public function __construct($list,$filename,$header)
   {
+    print "-----Merging for {$filename}-----\n";
     $merge = fopen($filename,"w");
     fwrite($merge,$header);
     foreach($list as $file)
     {
+      print "-----Merge in progress: {$file}-----\n";
       $lines = file($file);
       array_shift($lines);
+      print_r($lines);
       fwrite($merge,implode($lines));
     }
     fclose($merge);
@@ -20,3 +23,4 @@ class orrMergeExtract
 
 $test = new orrMergeExtract($config['Compliant'],$config['CompliantListName'],$config['orrExtract']);
 $test2 = new orrMergeExtract($config['NonCompliantList'],$config['NonCompliantListName'],$config['orrExtract']);
+?>
